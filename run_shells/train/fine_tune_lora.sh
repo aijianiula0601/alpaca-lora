36e1181f1441ef5e276b380ad4da96a9612a9895
aijianiula0601@gmail.com <348348jia>
2023-03-27 17:21:25 +0800

    update

---
+++ run_shells/train/fine_tune_lora.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+set -ex
+
+curdir=$(pwd)
+echo "curdir:$curdir"
+cd "$curdir" || exit
+
+cd ../../
+
+
+
+base_model_dir="/mnt/cephfs/hjh/train_record/nlp/stanford_alpaca/pretrain_models/llama/new_llama_7b"
+data_path="/mnt/cephfs/hjh/train_record/nlp/stanford_alpaca/ft_52k/alpaca_data_cleaned.json"
+
+output_dir="/mnt/cephfs/hjh/train_record/nlp/lora_stanford_alpaca/ft_52k/llama-7b-hf_fine_tune_out"
+
+CUDA_VISIBLE_DEVICES=6 \
+python finetune.py \
+    --base_model ${base_model_dir} \
+    --data_path ${data_path} \
+    --output_dir ${output_dir}
+
+
+#CUDA_VISIBLE_DEVICES=6 \
+#python finetune.py \
+#    --base_model ${base_model_dir} \
+#    --data_path ${data_path} \
+#    --output_dir ${output_dir} \
+#    --batch_size 128 \
+#    --micro_batch_size 4 \
+#    --num_epochs 3 \
+#    --learning_rate 1e-4 \
+#    --cutoff_len 512 \
+#    --val_set_size 2000 \
+#    --lora_r 8 \
+#    --lora_alpha 16 \
+#    --lora_dropout 0.05 \
+#    --lora_target_modules '[q_proj,v_proj]' \
+#    --train_on_inputs \
+#    --group_by_length
