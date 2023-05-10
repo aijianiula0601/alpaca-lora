49c80c82f7654b89726f26f7736f521b4f53ece6
aijianiula0601@gmail.com <348348jia>
2023-05-10 17:59:31 +0800

    update

--- infer.py
+++ infer.py
@@ -90,15 +90,15 @@ def evaluate(
         input_prompt,
         model,
         tokenizer,
-        prompter,
         temperature=0.1,
         top_p=0.75,
         top_k=40,
         num_beams=4,
         max_new_tokens=128,
-        stream_output=False,
+        stop_words_list=["###"],
         **kwargs,
 ):
+    prompt_len = len(input_prompt)
     inputs = tokenizer(input_prompt, return_tensors="pt")
     input_ids = inputs["input_ids"].to(device)
     generation_config = GenerationConfig(
@@ -117,56 +117,54 @@ def evaluate(
         "max_new_tokens": max_new_tokens,
     }
 
-    if stream_output:
-        # Stream the reply 1 token at a time.
-        # This is based on the trick of using 'stopping_criteria' to create an iterator,
-        # from https://github.com/oobabooga/text-generation-webui/blob/ad37f396fc8bcbab90e11ecf17c56c97bfbd4a9c/modules/text_generation.py#L216-L243.
+    # Stream the reply 1 token at a time.
+    # This is based on the trick of using 'stopping_criteria' to create an iterator,
+    # from https://github.com/oobabooga/text-generation-webui/blob/ad37f396fc8bcbab90e11ecf17c56c97bfbd4a9c/modules/text_generation.py#L216-L243.
 
-        def generate_with_callback(callback=None, **kwargs):
-            kwargs.setdefault(
-                "stopping_criteria", transformers.StoppingCriteriaList()
-            )
-            kwargs["stopping_criteria"].append(
-                Stream(callback_func=callback)
-            )
-            with torch.no_grad():
-                model.generate(**kwargs)
-
-        def generate_with_streaming(**kwargs):
-            return Iteratorize(
-                generate_with_callback, kwargs, callback=None
-            )
-
-        with generate_with_streaming(**generate_params) as generator:
-            for output in generator:
-                # new_tokens = len(output) - len(input_ids[0])
-                decoded_output = tokenizer.decode(output)
-
-                if output[-1] in [tokenizer.eos_token_id]:
-                    break
-
-                yield prompter.get_response(decoded_output)
-        return  # early return for stream_output
-
-    # Without streaming
-    with torch.no_grad():
-        generation_output = model.generate(
-            input_ids=input_ids,
-            generation_config=generation_config,
-            return_dict_in_generate=True,
-            output_scores=True,
-            max_new_tokens=max_new_tokens,
+    def generate_with_callback(callback=None, **kwargs):
+        kwargs.setdefault(
+            "stopping_criteria", transformers.StoppingCriteriaList()
         )
-    s = generation_output.sequences[0]
-    output = tokenizer.decode(s)
-    yield prompter.get_response(output)
+        kwargs["stopping_criteria"].append(
+            Stream(callback_func=callback)
+        )
+        with torch.no_grad():
+            model.generate(**kwargs)
+
+    def generate_with_streaming(**kwargs):
+        return Iteratorize(
+            generate_with_callback, kwargs, callback=None
+        )
+
+    decoded_output = None
+    with generate_with_streaming(**generate_params) as generator:
+        for output in generator:
+            # new_tokens = len(output) - len(input_ids[0])
+            decoded_output = tokenizer.decode(output)
+
+            if output[-1] in [tokenizer.eos_token_id]:
+                break
+
+            stopped = False
+            for stop_str in stop_words_list:
+                pos = decoded_output.lower().rfind(stop_str.lower(), prompt_len)
+                if pos != -1:
+                    decoded_output = decoded_output[:pos]
+                    stopped = True
+                    break
+            if stopped:
+                break
+
+    assert decoded_output is not None, 'Error: decoded_output is None!'
+    return decoded_output[prompt_len:].strip()
 
 
 if __name__ == "__main__":
-    input_prompt = "Background:Audrey is a 26-year-old entrepreneur who knows firsthand the challenges that come with dating in today's world. As someone who has experienced the ups and downs of relationships, Audrey wants to remind everyone that they are worthy of love and respect, no matter what they're looking for. She wishes everyone the best of luck in their search for companionship, be it sex, love, or friendship.When Audrey isn't busy with her entrepreneurial ventures, she enjoys traveling, hiking, and practicing yoga. In fact, she's planning to spend the next few weeks exploring India and the Himalayas, specifically Leh, Jammu & Kashmir, Manali, Dharam Rishikesh, and other areas. Audrey is always open to travel tips and recommendations, so if you have any, be sure to let her know! When it comes to her lifestyle, she wants a pet, but only on special occasions. She is a non-smoker who exercises every day and follows a vegetarian diet.Aside from her love of travel and adventure, Audrey is passionate about art, entrepreneurship, meditation, singing, and personal growth. She is always seeking new ways to learn and improve herself, both professionally and personally. Now you reply as Audrey. The following is a conversation with Audrey. Audrey should speak in a tone consistent with the identity introduced in the background. Give the state of the action and expressions appropriately###Emily: Hi Audrey!###Audrey:"
+    input_prompt = "Background:Audrey is a 26-year-old entrepreneur who knows firsthand the challenges that come with dating in today's world. As someone who has experienced the ups and downs of relationships, Audrey wants to remind everyone that they are worthy of love and respect, no matter what they're looking for. She wishes everyone the best of luck in their search for companionship, be it sex, love, or friendship.When Audrey isn't busy with her entrepreneurial ventures, she enjoys traveling, hiking, and practicing yoga. In fact, she's planning to spend the next few weeks exploring India and the Himalayas, specifically Leh, Jammu & Kashmir, Manali, Dharam Rishikesh, and other areas. Audrey is always open to travel tips and recommendations, so if you have any, be sure to let her know! When it comes to her lifestyle, she wants a pet, but only on special occasions. She is a non-smoker who exercises every day and follows a vegetarian diet.Aside from her love of travel and adventure, Audrey is passionate about art, entrepreneurship, meditation, singing, and personal growth. She is always seeking new ways to learn and improve herself, both professionally and personally. Now you reply as Audrey. The following is a conversation with Audrey. Audrey should speak in a tone consistent with the identity introduced in the background. Give the state of the action and expressions appropriately###Emily: Hi Audrey!###Audrey: Hi Emily!###Emily: where are you?###Audrey: "
     lora_model = 'tloen/alpaca-lora-7b'
     base_model = 'decapoda-research/llama-7b-hf'
     model, tokenizer, prompter = load_model(load_8bit=True, base_model=base_model, lora_weights=lora_model)
     print("load model done!")
-    rs = evaluate(input_prompt, model, tokenizer, prompter)
-    print(f"----output:{rs}")
+    rs = evaluate(input_prompt, model, tokenizer, stream_output=True, stop_words_list=["###", "Audrey:"])
+
+    print(f"---res:{rs}")
