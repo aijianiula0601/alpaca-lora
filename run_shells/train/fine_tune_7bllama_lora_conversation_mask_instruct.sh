fa94543313ba737a73c9ac2894969d757ed94213
aijianiula <aijianiula0601@gmail.com>
2023-05-12 07:04:17 +0800

    update

--- run_shells/train/fine_tune_7bllama_lora_conversation_mask_instruct.sh
+++ run_shells/train/fine_tune_7bllama_lora_conversation_mask_instruct.sh
@@ -17,8 +17,11 @@ your_random_port=11235
 #-------------------
 #多gpu训练
 #-------------------
-  torchrun --nproc_per_node=8 --master_port=${your_random_port} test_models/mask_instruct/finetune_mask_instruct.py \
+torchrun --nproc_per_node=8 --master_port=${your_random_port} test_models/mask_instruct/finetune_mask_instruct.py \
   --base_model 'decapoda-research/llama-7b-hf' \
   --data_path ${data_path} \
   --output_dir ${output_dir} \
-  --batch_size 64
+  --batch_size 64 \
+  --save_steps 500 \
+  --save_total_limit 10 \
+  --num_epochs 1
